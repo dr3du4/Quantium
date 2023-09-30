cf502cc84fbd13869f66f4d24298e1f16bfd65a6
Patryk Kurek <patrick@pop-os.localdomain>
2023-09-30 22:46:49 +0200

    feat: csv_to_scikit function ready to use :)

--- converter/csv_to_scikit.py
+++ converter/csv_to_scikit.py
@@ -1,38 +1,36 @@
 import numpy as np
 import pandas as pd
 
-input_file = "iris.csv"
+def csv_to_scikit(input_file):
+    df = pd.read_csv(input_file,header=0)
+    dictionary = {}
+    arr = []
 
-df = pd.read_csv(input_file,header=0)
-dictionary = {}
-arr = []
+    for ind in df[df.columns.values[len(df.columns.values)-1]].index:
+        word = df[df.columns.values[len(df.columns.values)-1]][ind]
+        arr.append(word)
 
-for ind in df[df.columns.values[len(df.columns.values)-1]].index:
-    word = df[df.columns.values[len(df.columns.values)-1]][ind]
-    arr.append(word)
+    arr = np.unique(arr)
+    print(arr)
 
-arr = np.unique(arr)
-print(arr)
+    dict = {}
+    for index,value in enumerate(arr):
+        dict[value]=index
+        
+    targets = []
+    for ind in df[df.columns.values[len(df.columns.values)-1]].index:
+        word=df[df.columns.values[len(df.columns.values)-1]][ind]
+        targets.append(dict[word])
 
-dict = {}
-for index,value in enumerate(arr):
-    dict[value]=index
-    
-targets = []
-for ind in df[df.columns.values[len(df.columns.values)-1]].index:
-    word=df[df.columns.values[len(df.columns.values)-1]][ind]
-    targets.append(dict[word])
+    original_headers = list(df.columns.values)
 
-original_headers = list(df.columns.values)
+    df = df._get_numeric_data()
 
-df = df._get_numeric_data()
+    numeric_headers = list(df.columns.values)
+    numpy_array = df.to_numpy()
 
-numeric_headers = list(df.columns.values)
-numpy_array = df.to_numpy()
+    numeric_headers.reverse()
+    reverse_df = df[numeric_headers] 
 
-numeric_headers.reverse()
-reverse_df = df[numeric_headers]
-
-print(targets)
-print(numpy_array)
-
+    d = {'data': numpy_array, 'target': targets}
+    return d
