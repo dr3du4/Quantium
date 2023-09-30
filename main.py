633afdeb60feaffe5a14d875fe94af61aa896354
dr3du4 <jan.krzyszkowski2@gmail.com>
2023-09-30 15:55:02 +0200

    main

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from qiskit.utils import algorithm_globals
 from sklearn.svm import SVC
+from qiskit.circuit.library import ZZFeatureMap
+from qiskit.circuit.library import RealAmplitudes
+from qiskit.algorithms.optimizers import COBYLA
+from qiskit.primitives import Sampler
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
+from qiskit_machine_learning.algorithms.classifiers import VQC
+import time
+from sklearn.decomposition import PCA
 
 iris_data = load_iris()
     #print(iris_data.DESCR)
@@ -36,3 +45,65 @@ train_features, test_features, train_labels, test_labels = train_test_split(
 
 svc = SVC()
 _ = svc.fit(train_features, train_labels)  # suppress printing the return value
+train_score_c4 = svc.score(train_features, train_labels)
+test_score_c4 = svc.score(test_features, test_labels)
+
+print(f"Classical SVC on the training dataset: {train_score_c4:.2f}")
+print(f"Classical SVC on the test dataset:     {test_score_c4:.2f}")
+
+
+num_features = features.shape[1]
+
+feature_map = ZZFeatureMap(feature_dimension=num_features, reps=1)
+feature_map.decompose().draw(output="mpl", fold=20)
+plt.show()
+
+ansatz = RealAmplitudes(num_qubits=num_features, reps=1)
+ansatz.decompose().draw(output="mpl", fold=20)
+plt.show()
+
+optimizer = COBYLA(maxiter=100)
+sampler = Sampler()
+
+
+objective_func_vals = []
+plt.rcParams["figure.figsize"] = (12, 6)
+
+
+def callback_graph(weights, obj_func_eval):
+    clear_output(wait=True)
+    objective_func_vals.append(obj_func_eval)
+    plt.title("Objective function value against iteration")
+    plt.xlabel("Iteration")
+    plt.ylabel("Objective function value")
+    plt.plot(range(len(objective_func_vals)), objective_func_vals)
+    plt.show()
+
+
+vqc = VQC(
+    sampler=sampler,
+    feature_map=feature_map,
+    ansatz=ansatz,
+    optimizer=optimizer,
+    callback=callback_graph,
+)
+
+# clear objective value history
+objective_func_vals = []
+
+start = time.time()
+vqc.fit(train_features, train_labels)
+elapsed = time.time() - start
+
+print(f"Training time: {round(elapsed)} seconds")
+
+train_score_q4 = vqc.score(train_features, train_labels)
+test_score_q4 = vqc.score(test_features, test_labels)
+
+print(f"Quantum VQC on the training dataset: {train_score_q4:.2f}")
+print(f"Quantum VQC on the test dataset:     {test_score_q4:.2f}")
+
+features = PCA(n_components=2).fit_transform(features)
+
+plt.rcParams["figure.figsize"] = (6, 6)
+sns.scatterplot(x=features[:, 0], y=features[:, 1], hue=labels, palette="tab10")
